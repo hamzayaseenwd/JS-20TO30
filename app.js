39255814cd5a4d5a5c777368c73e25772bc6c3d3
hamzayaseenwd <hamzayaseen836@gmail.com>
2023-09-25 20:02:55 +0500

    editing

--- app.js
+++ app.js
@@ -1,3 +1,57 @@
+// ---------------------- chapter 21 ---------------- //
+
+
 // Q1
+
+
+var allLower = userInput.toLowerCase();
+console.log(allLower)
+
+
+// Q2
+
+var x = prompt("Enter input")
+
+var allLower = x.toLowerCase();
+console.log(allLower)
+
+
+
+// Q3
+
+var y = prompt("Enter input")
+
+var allUpper = y.toUpperCase();
+
+console.log(allUpper)
+
+
+// Q4
+
+var string = prompt("Enter input")
+
+var toLowerCase = string.toLowerCase();
+
+console.log(toLowerCase)
+
+
+// Q5
+
+var arr = prompt["Enter in Arry"]
+
+var toLowerCase = arr[0].toLowerCase();
+
+console.log(toLowerCase)
+
+
+
+// Q6
+
+// var arr = ["Hello"]
+
+// var toLowerCase = arr[0].toLowerCase();
+
+// console.log(toLowerCase)
+
 
 
