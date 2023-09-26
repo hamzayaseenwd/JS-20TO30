2ad71a9fcb7c4f52651076734fb19d2d66981f0e
hamzayaseenwd <hamzayaseen836@gmail.com>
2023-09-26 16:59:14 +0500

    update

--- app.js
+++ app.js
@@ -47,11 +47,87 @@ console.log(toLowerCase)
 
 // Q6
 
-// var arr = ["Hello"]
 
-// var toLowerCase = arr[0].toLowerCase();
+var string = "Hello!"
 
-// console.log(toLowerCase)
+
+var upperCaseString = myString.toUpperCase();
+
+
+alert(upperCaseString);
+
+
+
+
+// Q7
+
+
+var userInput = "kaRacHi";
+
+console.log(userInput);
+
+var firstword = userInput.slice(0, 1).toUpperCase();
+
+var remainword = userInput.slice(1).toLowerCase();
+
+var completeWord = firstword + remainword;
+
+console.log(completeWord);
+
+
+
+// ---------------------- chapter 22 to chapter 25  ---------------- //
+
+
+// Q1
+
+var word = "captain";
+
+var slicedword = word.slice(1, 3);
+
+console.log(slicedword); 
+
+// Q2
+
+// var word = "captain";
+
+// Q3
+
+var string = "elephant";
+
+var seg = animal.slice(1, 5);
+
+console.log(seg);
+
+// Q4
+
+var word = prompt("Type any word")
+
+var length = word.length;
+
+console.log(length);
+
+// Q5
+
+// var word = prompt("Type any word")
+
+
+
+// Q6
+
+var text = "To be or not to be.";
+var indx = text.indexOf("be");
+
+console.log(indx)
+
+
+
+// Q7
+
+var text = "To be or not to be.";
+var indx = text.lastIndexOf("be");
+
+console.log(indx)
 
 
 
